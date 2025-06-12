c8fe6ad68b2ce7dad574d706e4cb6429dfa003a0
Ivan Duca <145754744+ivanduca@users.noreply.github.com>
2025-06-12 12:41:11 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ source_parsers = {
 
 project = "TrasparenzAI - Piattaforma per l'analisi e la consultazione della trasparenza amministrativa delle Pubbliche Amministrazioni"
 release = '1.0.0'
-author = u'CNR - ANAC'
+author = u'Consiglio Nazionale delle Ricerche'
 
 # Version è utilizzato per la produzione dell'ePUB
 version = release
